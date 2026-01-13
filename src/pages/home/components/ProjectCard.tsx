7b943e7515478c7a918691f800316376af594077
link200309 <jonhh.0023@gmail.com>
2026-01-12 21:06:19 -0400

    refactor(ProjectCard): improve ProjectCard

--- src/pages/home/components/ProjectCard.tsx
+++ src/pages/home/components/ProjectCard.tsx
@@ -1,14 +1,19 @@
 import { type Project } from "@/types";
-import ImageGallery from "@/components/imageGallery/ImageGallery";
+import screen4 from "@/assets/images/projects/projectTracker/screen4.webp";
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="relative bg-cover bg-center bg-no-repeat rounded-3xl border border-primary-pri3 card-border-ligth">
-      <div className="px-7 pt-7">
-        <h3 className="text-title-md">{project.title}</h3>
-        <p className="my-4">{project.description}</p>
+    <div className="relative h-64 border border-primary-pri3 card-border-ligth rounded cursor-pointer">
+      <img
+        src={screen4}
+        alt="imagen de tarjeta"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+
+      <div className="absolute z-10 p-4 bottom-0 w-full bg-black/50">
+        <h3 className="text-label-sm text-gray-400 ">Pagina web</h3>
+        <p className="text-white mt-2">Tracking for Dota 2</p>
       </div>
-      <ImageGallery />
     </div>
   );
 };
--- src/pages/home/components/Projects.tsx
+++ src/pages/home/components/Projects.tsx
@@ -10,43 +10,20 @@ import ProjectCard from "./ProjectCard";
 import { projects } from "@/data/Projects.data";
 
 const Projects = () => {
-  const projectsCol1 = useMemo(
-    () => projects.slice(0, projects.length / 2),
-    []
-  );
-  const projectsCol2 = useMemo(
-    () => projects.slice(projects.length / 2, projects.length),
-    []
-  );
-
   return (
     <section className="my-20">
       <SectionTitle title="Projects" />
-      <article className="max-w-3xl mt-10 grid grid-cols-2 gap-6">
-        <div className="grid gap-6">
-          {projectsCol1.map((project, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0 }}
-              transition={{ duration: 2, type: "spring" }}
-              whileInView={{ opacity: 1 }}
-            >
-              <ProjectCard project={project} />
-            </motion.div>
-          ))}
-        </div>
-        <div className="grid gap-6">
-          {projectsCol2.map((project, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0 }}
-              transition={{ duration: 2, type: "spring" }}
-              whileInView={{ opacity: 1 }}
-            >
-              <ProjectCard project={project} />
-            </motion.div>
-          ))}
-        </div>
+      <article className="max-w-3xl mt-10 grid grid-cols-3 gap-4">
+        {projects.map((project, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0 }}
+            transition={{ duration: 2, type: "spring" }}
+            whileInView={{ opacity: 1 }}
+          >
+            <ProjectCard project={project} />
+          </motion.div>
+        ))}
       </article>
     </section>
   );
