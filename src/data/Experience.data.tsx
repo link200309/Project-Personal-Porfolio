7f723bb1d5eee5aacd030cb4f02492a1d1b0aa62
link200309 <jonhh.0023@gmail.com>
2026-01-16 22:39:54 -0400

    refactor(ProjectData): add image galley for each project

--- src/data/Experience.data.tsx
+++ src/data/Experience.data.tsx
@@ -8,6 +8,46 @@ type Experience = {
 
 export const experiences: Experience[] = [
   {
+    title: "DeosCorp SRL. - Full Stack Developer",
+    date: "Agosto 2025 - Octubre 2025",
+    descriptions: [
+      <>
+        I worked developing interfaces with ,{" "}
+        <span className="text-primary-pri1 font-semibold">
+          React, TypeScript, Tailwind CSS, and Shadcn
+        </span>
+        .
+      </>,
+      <>
+        I develop endpoints with{" "}
+        <span className="text-primary-pri1 font-semibold">Express</span>, for
+        various functionalities.
+      </>,
+      <>
+        I developed interactive tables with{" "}
+        <span className="text-primary-pri1 font-semibold">Tanstack Table</span>.
+      </>,
+      <>
+        I develop searches and filters through the{" "}
+        <span className="text-primary-pri1 font-semibold">server</span>{" "}
+      </>,
+      <>
+        I develop{" "}
+        <span className="text-primary-pri1 font-semibold">pagination </span>{" "}
+        using the server integrated with{" "}
+        <span className="text-primary-pri1 font-semibold">tanstack table</span>.
+      </>,
+      <>
+        I develop Dashboards with Charts using{" "}
+        <span className="text-primary-pri1 font-semibold">Chart.js</span>.
+      </>,
+      <>
+        I develop authentication based on{" "}
+        <span className="text-primary-pri1 font-semibold">JWT</span>.
+      </>,
+    ],
+  },
+  {
     title: "University of San Simon - System Engineer",
     date: "Febrero 2022 - PRESENTE",
     descriptions: [
@@ -39,14 +79,6 @@ export const experiences: Experience[] = [
         collaboration with various tools and technologies.
       </>,
       <>
-        I acquired extensive{" "}
-        <span className="text-primary-pri1 font-semibold">mathematical</span>{" "}
-        and{" "}
-        <span className="text-primary-pri1 font-semibold">statistical </span>
-        knowledge, which allows me to create innovative solutions in
-        collaboration with various tools and technologies.
-      </>,
-      <>
         I developed efficient and robust{" "}
         <span className="text-primary-pri1 font-semibold">
           web applications
@@ -60,47 +92,6 @@ export const experiences: Experience[] = [
           mobile applications
         </span>
         .
-      </>,
-    ],
-  },
-
-  {
-    title: "DeosCorp SRL. - Full Stack Developer",
-    date: "Agosto 2025 - Octubre 2025",
-    descriptions: [
-      <>
-        I worked developing interfaces with ,{" "}
-        <span className="text-primary-pri1 font-semibold">
-          React, Tailwind CSS, and Shadcn
-        </span>
-        .
-      </>,
-      <>
-        I develop endpoints with{" "}
-        <span className="text-primary-pri1 font-semibold">Express</span>, for
-        various functionalities.
-      </>,
-      <>
-        I developed interactive tables with{" "}
-        <span className="text-primary-pri1 font-semibold">Tanstack Table</span>.
-      </>,
-      <>
-        I develop searches and filters through the{" "}
-        <span className="text-primary-pri1 font-semibold">server</span>{" "}
-      </>,
-      <>
-        I develop pagination using the
-        <span className="text-primary-pri1 font-semibold">server </span>
-        integrated with{" "}
-        <span className="text-primary-pri1 font-semibold">tanstack table</span>.
-      </>,
-      <>
-        I develop Dashboards with Charts using{" "}
-        <span className="text-primary-pri1 font-semibold">Chart.js</span>.
-      </>,
-      <>
-        I develop authentication based on{" "}
-        <span className="text-primary-pri1 font-semibold">JWT</span>.
       </>,
     ],
   },
--- src/data/Projects.data.ts
+++ src/data/Projects.data.ts
@@ -10,7 +10,49 @@ import laravelLogo from "@/assets/images/technologies/logo-laravel.svg";
 import pythonLogo from "@/assets/images/technologies/logo-python.svg";
 import djangoLogo from "@/assets/images/technologies/logo-django.svg";
 import javaLogo from "@/assets/images/technologies/logo-java.svg";
-import project1 from "@/assets/images/projects/sampleProject.png";
+
+import trackingDotaScreen1 from "@/assets/images/projects/projectTracker/screen1.webp";
+import trackingDotaScreen2 from "@/assets/images/projects/projectTracker/screen2.webp";
+import trackingDotaScreen3 from "@/assets/images/projects/projectTracker/screen3.webp";
+import trackingDotaScreen4 from "@/assets/images/projects/projectTracker/screen4.webp";
+import trackingDotaScreen5 from "@/assets/images/projects/projectTracker/screen5.webp";
+
+import busScreen1 from "@/assets/images/projects/projectBus/screen1.png";
+import busScreen2 from "@/assets/images/projects/projectBus/screen2.png";
+import busScreen3 from "@/assets/images/projects/projectBus/screen3.png";
+import busScreen4 from "@/assets/images/projects/projectBus/screen4.png";
+import busScreen5 from "@/assets/images/projects/projectBus/screen5.png";
+
+import teachingScreen1 from "@/assets/images/projects/projectPythonTeaching/screen1.png";
+import teachingScreen2 from "@/assets/images/projects/projectPythonTeaching/screen2.png";
+import teachingScreen3 from "@/assets/images/projects/projectPythonTeaching/screen3.png";
+import teachingScreen4 from "@/assets/images/projects/projectPythonTeaching/screen4.png";
+import teachingScreen5 from "@/assets/images/projects/projectPythonTeaching/screen5.png";
+import teachingScreen6 from "@/assets/images/projects/projectPythonTeaching/screen6.png";
+
+import olympicScreen1 from "@/assets/images/projects/projectOlympics/screen1.jpg";
+import olympicScreen2 from "@/assets/images/projects/projectOlympics/screen2.png";
+import olympicScreen3 from "@/assets/images/projects/projectOlympics/screen3.png";
+import olympicScreen4 from "@/assets/images/projects/projectOlympics/screen4.png";
+import olympicScreen5 from "@/assets/images/projects/projectOlympics/screen5.png";
+import olympicScreen6 from "@/assets/images/projects/projectOlympics/screen6.png";
+
+import sambaScreen1 from "@/assets/images/projects/projectSambaManager/screen1.png";
+import sambaScreen2 from "@/assets/images/projects/projectSambaManager/screen2.png";
+import sambaScreen3 from "@/assets/images/projects/projectSambaManager/screen3.png";
+import sambaScreen4 from "@/assets/images/projects/projectSambaManager/screen4.png";
+
+import rentingScreen1 from "@/assets/images/projects/projectRentingFriends/screen1.jpg";
+import rentingScreen2 from "@/assets/images/projects/projectRentingFriends/screen2.jpg";
+import rentingScreen3 from "@/assets/images/projects/projectRentingFriends/screen3.jpg";
+import rentingScreen4 from "@/assets/images/projects/projectRentingFriends/screen4.jpg";
+
+import gameScreen1 from "@/assets/images/projects/projectDesktopGames/screen1.png";
+import gameScreen2 from "@/assets/images/projects/projectDesktopGames/screen2.png";
+import gameScreen3 from "@/assets/images/projects/projectDesktopGames/screen3.png";
+import gameScreen4 from "@/assets/images/projects/projectDesktopGames/screen4.png";
+import gameScreen5 from "@/assets/images/projects/projectDesktopGames/screen5.png";
+
 import { type Project } from "@/types";
 
 export const projects: Project[] = [
@@ -20,7 +62,13 @@ export const projects: Project[] = [
       "Participé en el desarrollo del sistema, con un enfoque en la creación de interfaces, endopoints y el GSI (Game State Integration) para el juego.",
     link: "",
     application: "Web Page",
-    image: project1,
+    gallery: [
+      trackingDotaScreen1,
+      trackingDotaScreen2,
+      trackingDotaScreen3,
+      trackingDotaScreen4,
+      trackingDotaScreen5,
+    ],
     technologies: [
       {
         title: "React",
@@ -45,7 +93,7 @@ export const projects: Project[] = [
     description:
       "Encargado del diseño y desarrollo de las interfaces de usuario, así como la creación de APIs.",
     link: "",
-    image: "",
+    gallery: [busScreen1, busScreen2, busScreen3, busScreen4, busScreen5],
     application: "Mobile App",
     technologies: [
       {
@@ -67,7 +115,14 @@ export const projects: Project[] = [
     description:
       "Responsable del diseño y desarrollo de las interfaces de usuario y de la implementación de un editor de código interactivo para ejercicios prácticos.",
     link: "",
-    image: "",
+    gallery: [
+      teachingScreen1,
+      teachingScreen2,
+      teachingScreen3,
+      teachingScreen4,
+      teachingScreen5,
+      teachingScreen6,
+    ],
     application: "Web Page",
     technologies: [
       {
@@ -93,7 +148,14 @@ export const projects: Project[] = [
     description:
       "Me encargué principalmente de la inscripción manual, validaciones de formularios y las APIs necesarias para su funcionamiento.",
     link: "",
-    image: project1,
+    gallery: [
+      olympicScreen1,
+      olympicScreen2,
+      olympicScreen3,
+      olympicScreen4,
+      olympicScreen5,
+      olympicScreen6,
+    ],
     application: "Web Page",
     technologies: [
       {
@@ -115,7 +177,7 @@ export const projects: Project[] = [
     description:
       "Desarrollé la interfaz gráfica y la ejecución de comandos del sistema.",
     link: "",
-    image: "",
+    gallery: [sambaScreen1, sambaScreen2, sambaScreen3, sambaScreen4],
     application: "Desktop App",
     technologies: [
       {
@@ -133,7 +195,7 @@ export const projects: Project[] = [
     description:
       "Trabajé tanto en la creación de interfaces como en el desarrollo de APIs para la comunicación entre módulos.",
     link: "",
-    image: "",
+    gallery: [rentingScreen1, rentingScreen2, rentingScreen3, rentingScreen4],
     application: "Web Page",
     technologies: [
       {
@@ -155,7 +217,7 @@ export const projects: Project[] = [
     description:
       "Desarrollé juegos como tres en raya, serpiente y plataformas 2D simples.",
     link: "",
-    image: "",
+    gallery: [gameScreen1, gameScreen2, gameScreen3, gameScreen4, gameScreen5],
     application: "Desktop App",
     technologies: [
       {
